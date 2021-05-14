5cc59fc5f5c41e58de76af276c7b7c81ed45d636
ANKIT KENE <47526753+keneankit01@users.noreply.github.com>
2021-05-14 20:44:19 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+mkdir mywork
+cd mywork
+apt-get update
+apt-get install sudo -y
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt install -y git git-core
+git config --global user.name "keneankit01"
+git config --global user.email "keneankit01@gmail.com"
+git config --global color.ui false
