7348c7fe7a52982fd94f53c3cc8854e81ba877d0
ANKIT KENE <47526753+keneankit01@users.noreply.github.com>
2021-05-14 21:45:17 +0530

    python3-pip

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install sudo -y
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt install -y git git-core python3 python
+sudo apt install -y git git-core python3 python python3-pip
 git config --global user.name "keneankit01"
 git config --global user.email "keneankit01@gmail.com"
 git config --global color.ui false
