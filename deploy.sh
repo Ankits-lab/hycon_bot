01102bc551a251e58ba026821b06d5cb2dbfadfd
ANKIT KENE <47526753+keneankit01@users.noreply.github.com>
2021-05-14 21:43:13 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 cd mywork
 git clone https://github.com/Ankits-lab/HyconBot.git Bot
 cd Bot
-apt-get install python3-pip
+apt-get install -y python3-pip 
 python3 pip install -m requirements.txt
 python3 -m hycon
