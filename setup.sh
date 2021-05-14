28cca1aef8821db5bd60b9456902d97cb94d4993
ANKIT KENE <47526753+keneankit01@users.noreply.github.com>
2021-05-14 20:57:41 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install sudo -y
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt install -y git git-core
+sudo apt install -y git git-core python3 python
 git config --global user.name "keneankit01"
 git config --global user.email "keneankit01@gmail.com"
 git config --global color.ui false
